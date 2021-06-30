809fb49110679eaa01eb71323c6ed981eaa8a1b2
aristhemage <49156381+aristhemage@users.noreply.github.com>
2021-06-30 12:46:33 -0400

    Update script.js

--- script.js
+++ script.js
@@ -3,7 +3,9 @@ var radios = document.getElementsByName('time');
 for (var i = 0, length = radios.length; i < length; i++) {
   if (radios[i].checked) {
     // do whatever you want with the checked radio
-    alert(radios[i].value);
+    if (radios[i].value == 11){
+  document.write("Hello World!");
+    }
 
     // only one radio can be logically checked, don't check the rest
     break;
